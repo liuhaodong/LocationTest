ec5227571a69f5e4f000d523704cd0b875029280
Haodong Liu <hi.haodong@gmail.com>
2018-04-15 02:07:05 -0700

    update

--- location/src/main/java/com/google/haodongl/locationtest/location/LocationService.java
+++ location/src/main/java/com/google/haodongl/locationtest/location/LocationService.java
@@ -1,20 +1,47 @@
 package com.google.haodongl.locationtest.location;
 
+import android.Manifest;
 import android.app.IntentService;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 public class LocationService extends IntentService {
 
-    public LocationService(){
-        super("haodong's service");
+  private FusedLocationProviderClient mFusedLocationClient;
+
+  public LocationService() {
+    super("haodong's service");
+  }
+
+  @Override
+  protected void onHandleIntent(@Nullable Intent intent) {
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+      // TODO: Consider calling
+      //    ActivityCompat#requestPermissions
+      // here to request the missing permissions, and then overriding
+      //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+      //                                          int[] grantResults)
+      // to handle the case where the user grants the permission. See the documentation
+      // for ActivityCompat#requestPermissions for more details.
+//      return;
     }
 
-    @Override
-    protected void onHandleIntent(@Nullable Intent intent) {
-        String data = intent.getDataString();
-        Log.i("location", data);
-        System.exit(0);
-    }
+
+    this.mFusedLocationClient =
+        LocationServices.getFusedLocationProviderClient(getApplicationContext());
+    mFusedLocationClient.getLastLocation().addOnCompleteListener(location -> {
+      Log.i("location", location.getResult().toString());
+    });
+    Log.i("location", "success");
+  }
 }
--- location/src/main/java/com/google/haodongl/locationtest/location/MainActivity.java
+++ location/src/main/java/com/google/haodongl/locationtest/location/MainActivity.java
@@ -1,35 +1,41 @@
 package com.google.haodongl.locationtest.location;
 
+import android.Manifest;
 import android.content.ClipData;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ActivityCompat.requestPermissions(this, new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION }, 1);
 
-        Intent locationServiceIntent = new Intent(this.getApplicationContext(), LocationService.class);
-        locationServiceIntent.setData(Uri.EMPTY);
-        getApplicationContext().startService(locationServiceIntent);
-
-        Button startButton = findViewById(R.id.button);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
+    Button startButton = findViewById(R.id.button);
+    startButton.setOnClickListener(
+        (v) -> {
+          Log.i("main_activity", "test message");
+          startLocationService();
         });
-    }
+  }
 
-    @Override
-    protected void onStart(){
-        super.onStart();
-    }
+  private void startLocationService() {
+    Intent locationServiceIntent = new Intent(this.getApplicationContext(), LocationService.class);
+    locationServiceIntent.setData(Uri.EMPTY);
+    getApplicationContext().startService(locationServiceIntent);
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+  }
 }
